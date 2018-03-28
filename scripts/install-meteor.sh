70e6180cbe8279319a52ec64868028ba883a9ebc
Jack Kavanagh <jackkav@gmail.com>
2018-03-28 15:51:41 +0800

    meteor 1.6.1

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -21,8 +21,10 @@ else
   # https://github.com/jshimko/meteor-launchpad/issues/39
   #sed -i.bak "s/tar -xzf.*/bsdtar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" /tmp/install_meteor.sh
 
-  mkdir tmp/.meteor-install-tmp
-  curl --progress-bar --insecure  https://meteorinstall-4168.kxcdn.com/packages-bootstrap/1.5.1/meteor-bootstrap-os.linux.x86_64.tar.gz | tar -xzf - -C "tmp/.meteor-install-tmp" -o
+  printf "\n[-] Downloading Meteor $METEOR_VERSION...\n\n"
+  mkdir /tmp/.meteor-install-tmp
+  curl --progress-bar --insecure  https://meteorinstall-4168.kxcdn.com/packages-bootstrap/1.6.1/meteor-bootstrap-os.linux.x86_64.tar.gz | tar -xzf - -C "tmp/.meteor-install-tmp" -o
+  #wget -t 70 -c https://meteorinstall-4168.kxcdn.com/packages-bootstrap/1.6.1/meteor-bootstrap-os.linux.x86_64.tar.gz -O - | tar -xzf - -C "/tmp/.meteor-install-tmp"
 
 
   # install
