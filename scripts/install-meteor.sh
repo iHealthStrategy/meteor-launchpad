e6f12bf65900f11f677a8db0c23991c1793e5c73
Jack Kavanagh <jackkav@gmail.com>
2018-03-28 17:26:19 +0800

    hmm

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -8,8 +8,10 @@ if [ "$DEV_BUILD" = true ]; then
   printf "\n[-] Installing the latest version of Meteor...\n\n"
   curl -v https://install.meteor.com/ | sh
 else
+  printf "\n[-] Downloading Meteor $METEOR_VERSION...\n\n"
+  mkdir /tmp/.meteor-install-tmp
   # download installer script
-  #curl -v https://install.meteor.com -o /tmp/install_meteor.sh
+  curl -v https://install.meteor.com -o /tmp/install_meteor.sh
 
   # read in the release version in the app
   #METEOR_VERSION=$(head $APP_SOURCE_DIR/.meteor/release | cut -d "@" -f 2)
@@ -21,9 +23,8 @@ else
   # https://github.com/jshimko/meteor-launchpad/issues/39
   #sed -i.bak "s/tar -xzf.*/bsdtar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" /tmp/install_meteor.sh
 
-  printf "\n[-] Downloading Meteor $METEOR_VERSION...\n\n"
-  mkdir /tmp/.meteor-install-tmp
-  curl --progress-bar --insecure  https://meteorinstall-4168.kxcdn.com/packages-bootstrap/1.6.1/meteor-bootstrap-os.linux.x86_64.tar.gz | tar -xzf - -C "tmp/.meteor-install-tmp" -o
+
+  #curl --progress-bar --insecure  https://meteorinstall-4168.kxcdn.com/packages-bootstrap/1.6.1/meteor-bootstrap-os.linux.x86_64.tar.gz | tar -xzf - -C "tmp/.meteor-install-tmp" -o
   #wget -t 70 -c https://meteorinstall-4168.kxcdn.com/packages-bootstrap/1.6.1/meteor-bootstrap-os.linux.x86_64.tar.gz -O - | tar -xzf - -C "/tmp/.meteor-install-tmp"
 
 
