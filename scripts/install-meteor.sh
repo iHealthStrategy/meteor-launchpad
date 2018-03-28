91eee350257148b4300bcdb79d826fac24e502a4
Jack Kavanagh <jackkav@gmail.com>
2018-03-28 18:06:42 +0800

    alt dl url

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -19,11 +19,15 @@ else
   # set the release version in the install script
   sed -i.bak "s/RELEASE=.*/RELEASE=\"$METEOR_VERSION\"/g" /tmp/install_meteor.sh
 
+  # use alternate url
+  sed -i.bak "s/static-meteor.netdna-ssl.com/meteorinstall-4168.kxcdn.com/g" /tmp/install_meteor.sh
+
   # replace tar command with bsdtar in the install script (bsdtar -xf "$TARBALL_FILE" -C "$INSTALL_TMPDIR")
   # https://github.com/jshimko/meteor-launchpad/issues/39
   sed -i.bak "s/tar -xzf.*/bsdtar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" /tmp/install_meteor.sh
 
   #mkdir /tmp/.meteor-install-tmp
+  #https://pan.baidu.com/s/1U-gxEZbNtIW2Xg4eizKAkA
   #curl --progress-bar --insecure  https://meteorinstall-4168.kxcdn.com/packages-bootstrap/1.6.1/meteor-bootstrap-os.linux.x86_64.tar.gz | tar -xzf - -C "tmp/.meteor-install-tmp" -o
   #wget -t 70 -c https://meteorinstall-4168.kxcdn.com/packages-bootstrap/1.6.1/meteor-bootstrap-os.linux.x86_64.tar.gz -O - | tar -xzf - -C "/tmp/.meteor-install-tmp"
 
